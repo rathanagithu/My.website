3011241307dd5e6e1918cba3b6028f1ba16a53b6
rathanathon.githu.io <154632753+rathanagithu@users.noreply.github.com>
2024-01-10 04:26:00 +0700

    Create Package.js
    Signed-off-by: rathanathon.githu.io <154632753+rathanagithu@users.noreply.github.com>

---
+++ Package.js
@@ -0,0 +1,22 @@
+//npm.pkg.github.com/:_authToken=TOKEN
+$ npm login --scope=@NAMESPACE --auth-type=legacy --registry=https://npm.pkg.github.com
+
+> Username: USERNAME
+> Password: TOKEN
+@NAMESPACE:registry=https://npm.pkg.github.com
+repository":"https://github.com/OWNER/REPOSITORY",
+@NAMESPACE:registry=https://npm.pkg.github.com
+{
+  "name": "@my-org/server",
+  "version": "1.0.0",
+  "description": "Server app that uses the ORGANIZATION_NAME/PACKAGE_NAME package",
+  "main": "index.js",
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+  "ORGANIZATION_NAME/PACKAGE_NAME": "1.0.0"
+}
+ npm install
+ @NAMESPACE:registry=https://npm.pkg.github.com
+ @NAMESPACE:registry=https://npm.pkg.github.com
+ ](https://myoctocat.com/assets/images/base-
